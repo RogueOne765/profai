5be911a6022c00fdc9c7ebfcfb17d54d3820fd67
Luca Menghini <luca.menghini@mashfrog.com>
2026-07-01 16:49:50 +0200

    fix path requirements.txt

--- ci_cd_pipeline/scripts/install_python_env.sh
+++ ci_cd_pipeline/scripts/install_python_env.sh
@@ -4,5 +4,4 @@ set -e
 API_SOURCE_PATH="$WORKSPACE"/"ci_cd_pipeline/api"
 
 #installa dipendenze
-pip3 install --quiet --no-cache-dir --break-system-packages pylint pylint-json2html pip-audit
 pip3 install --quiet --no-cache-dir --break-system-packages -r "$API_SOURCE_PATH"/requirements.txt
