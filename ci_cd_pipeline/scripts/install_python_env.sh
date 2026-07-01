f13248610936e8deabd947f99cf924bc481ccc41
Luca Menghini <luca.menghini@mashfrog.com>
2026-07-01 11:16:34 +0200

    fix directory script

--- ci_cd_pipeline/scripts/install_python_env.sh
+++ ci_cd_pipeline/scripts/install_python_env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pip3 install --quiet --no-cache-dir --break-system-packages nosexcover pylint "$WORKSPACE"/ci_cd_pipeline/requirements.txt
-nosetests --with-xcoverage --with-xunit --cover-package="$WORKSPACE"/app --cover-erase
+pip3 install --quiet --no-cache-dir --break-system-packages pytest pylint "$WORKSPACE"/ci_cd_pipeline/requirements.txt
 pylint -f parseable "$WORKSPACE"/app | tee pylint.out
