9069aa8753b5395f862780c3446f6ec44f9c704d
Luca Menghini <luca.menghini@mashfrog.com>
2026-07-01 11:06:02 +0200

    aggiorna script installazione dip python

--- ci_cd_pipeline/scripts/install_python_env.sh
+++ ci_cd_pipeline/scripts/install_python_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 install --quiet --no-cache-dir --break-system-packages nosexcover pylint "$WORKSPACE"/requirements.txt
+pip3 install --quiet --no-cache-dir --break-system-packages nosexcover pylint "$WORKSPACE"/ci_cd_pipeline/requirements.txt
 nosetests --with-xcoverage --with-xunit --cover-package="$WORKSPACE"/app --cover-erase
 pylint -f parseable "$WORKSPACE"/app | tee pylint.out
