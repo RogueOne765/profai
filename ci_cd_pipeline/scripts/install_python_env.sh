ae03f69abb6c30f79730a8e7850d27eb5728684c
Luca Menghini <luca.menghini@mashfrog.com>
2026-07-01 11:22:40 +0200

    const path api

--- ci_cd_pipeline/scripts/install_python_env.sh
+++ ci_cd_pipeline/scripts/install_python_env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+API_SOURCE_PATH="$WORKSPACE"/"ci_cd_pipeline/api"
+
 #installa dipendenze
 pip3 install --quiet --no-cache-dir --break-system-packages pytest pylint
-pip3 install --quiet --no-cache-dir --break-system-packages -r "$WORKSPACE"/ci_cd_pipeline/api/requirements.txt
+pip3 install --quiet --no-cache-dir --break-system-packages -r "$API_SOURCE_PATH"/requirements.txt
 
 # esegue pylint
-pylint -f parseable "$WORKSPACE"/app | tee pylint.out
+pylint -f parseable "$API_SOURCE_PATH" | tee pylint.out
