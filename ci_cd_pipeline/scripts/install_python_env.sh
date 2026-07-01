63f13fbdb17443fb6bde5e8210f4058fd68f6fd1
Luca Menghini <luca.menghini@mashfrog.com>
2026-07-01 11:20:22 +0200

    fix param installazione dip

--- ci_cd_pipeline/scripts/install_python_env.sh
+++ ci_cd_pipeline/scripts/install_python_env.sh
@@ -2,7 +2,8 @@
 set -e
 
 #installa dipendenze
-pip3 install --quiet --no-cache-dir --break-system-packages pytest pylint "$WORKSPACE"/ci_cd_pipeline/api/requirements.txt
+pip3 install --quiet --no-cache-dir --break-system-packages pytest pylint
+pip3 install --quiet --no-cache-dir --break-system-packages -r "$WORKSPACE"/ci_cd_pipeline/api/requirements.txt
 
 # esegue pylint
 pylint -f parseable "$WORKSPACE"/app | tee pylint.out
