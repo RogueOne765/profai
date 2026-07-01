263733d843ed6237e752460e744b87ec86fb6343
Luca Menghini <luca.menghini@mashfrog.com>
2026-07-01 16:55:47 +0200

    aggiorna path script build

--- ci_cd_pipeline/scripts/install_python_env.sh
+++ ci_cd_pipeline/scripts/install_python_env.sh
@@ -4,4 +4,5 @@ set -e
 API_SOURCE_PATH="$WORKSPACE"/"ci_cd_pipeline/api"
 
 #installa dipendenze
+pip3 install --no-cache-dir --break-system-packages pylint pylint-json2html pip-audit
 pip3 install --quiet --no-cache-dir --break-system-packages -r "$API_SOURCE_PATH"/requirements.txt
