7ef1ff0584b612924aecba043d847333b6c1a1fe
Luca Menghini <luca.menghini@mashfrog.com>
2026-07-01 11:02:04 +0200

    aggiunge container agent

---
+++ ci_cd_pipeline/README.md
@@ -0,0 +1,5 @@
+- creare nodo in dashboard jenkins
+- copiare nome e secret del nodo creato e inserirli nel file .env
+- riavviare il container dell'agente jenkins
+- nella configurazione del nodo "master", abbassare numero esecutori a 0
+
--- ci_cd_pipeline/scripts/install_python_env.sh
+++ ci_cd_pipeline/scripts/install_python_env.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-PYENV_HOME=$WORKSPACE/.pyenv/
-
-# Delete previously built virtualenv
-if [ -d "$PYENV_HOME" ]; then
-    rm -rf "$PYENV_HOME"
-fi
-
-# Create virtualenv and install necessary packages
-virtualenv --no-site-packages "$PYENV_HOME"
-. "$PYENV_HOME"/bin/activate
-pip install --quiet nosexcover
-pip install --quiet pylint
-pip install --quiet "$WORKSPACE"/requirements.txt
+pip3 install --quiet --no-cache-dir --break-system-packages nosexcover pylint "$WORKSPACE"/requirements.txt
 nosetests --with-xcoverage --with-xunit --cover-package="$WORKSPACE"/app --cover-erase
 pylint -f parseable "$WORKSPACE"/app | tee pylint.out
